2b6b4cd5b50d1dd2952af73a882a27db4e10e63b
thomasperson <nateperson@yahoo.com>
2018-07-23 17:27:11 -0400

    parses unstructed for lemmad ngrams for phrase anaysis

---
+++ NLP/nltk.parse.py
@@ -0,0 +1,18 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+def parsePhrase(text):
+
+
+def main():
+    with open('../other_dataSampleTxt.txt') as fin:
+        for line in fin:
+        pass # do something
+
+
+
+if __name__ == '__main__':
+	main()
+	exit()
