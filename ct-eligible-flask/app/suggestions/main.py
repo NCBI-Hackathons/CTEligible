bbb0a9bc5b2cd9a9bd291b8febafdf0f9c948bd9
juliomarcopineda <juliomarcopineda@gmail.com>
2018-09-11 13:10:26 -0400

    testing other input text

--- ct-eligible-flask/app/suggestions/main.py
+++ ct-eligible-flask/app/suggestions/main.py
@@ -3,11 +3,14 @@ import pprint
 
 
 def main():
-    input_text = 'The platelet count is greater than 10000'
+    input_text_1 = 'The platelet greater than or equal to 70000 platelets per cubic millimeter'
+    input_text_2 = 'Within 36 days prior to registration, you must not have treatment'
     suggestor = ClusterSuggestor(from_mongo=False)
-    suggestions = suggestor.suggest(input_text)
+    suggestions_1 = suggestor.suggest(input_text_1)
+    suggestions_2 = suggestor.suggest(input_text_2)
 
-    pprint.pprint(suggestions)
+    pprint.pprint(suggestions_1)
+    pprint.pprint(suggestions_2)
 
 
 if __name__ == '__main__':
