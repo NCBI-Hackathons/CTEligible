15dfc89067076ac05ce3d4d5a4fbc0824a43d995
juliomarcopineda <juliomarcopineda@gmail.com>
2018-09-11 11:07:35 -0400

    more tfidf calculations

--- ct-eligible-flask/app/suggestions/tfidf_converter.py
+++ ct-eligible-flask/app/suggestions/tfidf_converter.py
@@ -7,7 +7,29 @@ class TfidfConverter:
         """
         self.freq_vectors = freq_vectors
         self.term_freq = {}
+        self.doc_freq = {}
         self.idf = {}
+        self.tfidf = {}
 
     def generate_tfidf_vectors(self):
-        for cluster, freq_vector in freq_vectors.items():
+        # Calculate TF and DF
+        self.populate_term_doc_freq()
+
+        # Calculate IDF
+
+        # Calculate TF-IDF
+
+    def populate_term_doc_freq(self):
+        for cluster, freq_vector in self.freq_vectors.items():
+            total = float(sum(freq for freq in freq_vector.values()))
+
+            freq_dict = {}
+            for term, count in freq_vector.items():
+                freq_dict[term] = count / total
+
+                if term in self.doc_freq:
+                    self.doc_freq[term] = self.doc_freq[term] + 1.0
+                else:
+                    self.doc_freq[term] = 1.0
+
+            self.term_freq[cluster] = freq_dict
